13450e7db8b0f728937d454b4747097b1c667c88
Felipemacedo315 <felipemacedo315@gmail.com>
2021-12-19 17:02:13 -0300

    add funcionalidade e melhora no layout

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+var title
+function capture_numbers1(num) {
+
+ title= document.getElementById('resultado').innerHTML += num
+  
+
+}
+
+function result() {
+    
+ var result = eval(title)
+  document.getElementById('resultado').innerText=result
+}
+
+function clean() {
+  document.getElementById('resultado').innerText=''
+}
+function del() {
+  var del= document.getElementById('resultado').innerText
+   var excluir = del.slice(0,-1)
+   document.getElementById('resultado').innerText=excluir
+}
